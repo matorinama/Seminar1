6f8fad0c1e9706f1974ba2c68f4dc58662fbe055
Mari <matorina.mariya@mail.ru>
2023-06-15 22:41:33 +0300

    задача7

---
+++ Задача7/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Задача 7. Напишите программу, которая на вход принимает тёхзначное число 
+и на выходе показывает последнюю цифру этого числа*/
+
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int N = int.Parse(Console.ReadLine());
+int ostN = N % 10;
+Console.WriteLine(ostN);
